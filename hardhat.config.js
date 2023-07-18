8f800e6012aa201646573472b634a15053853d4f
Vikas Phulriya <88418134+vikasphulariya@users.noreply.github.com>
2023-07-19 01:50:59 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
         hardhat: {},
         rospten: { 
             url: "https://eth-goerli.g.alchemy.com/v2/822oQpZ5OrMUvLi2iGTTHHCuL0yABhe_",
-            accounts: [`daacf807221af27adb38ecabd3fa2d2d38b73922a04287a316307cf9f9975909`],
+            accounts: [`${process.env.PRIVATE_KEY}`],
            },
        },
   };
